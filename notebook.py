c2dde9ac37161282763d8839d13941e87aae41c9
t4w <lorenz.burgard@gmail.com>
2026-06-15 17:46:44 +0200

    just some random learning scripts for deeper understanding.

--- notebook.py
+++ notebook.py
@@ -5,6 +5,16 @@ linear = nn.Linear(1,1)
 with torch.no_grad():
     linear.weight.fill_(2.0)
     linear.bias.fill_(1.0)
+    
+x = torch.tensor([5.0])
+loss = linear(x).sum()
+
+print(f"Vor backward: {linear.weight.grad}")
+loss.backward()
+print(f"Nach backward: {linear.weight.grad}")
 
 
+print(loss.grad_fn)
+print(linear.weight.grad_fn)
 
+print(linear.parameters())
